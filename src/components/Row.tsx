456c65062171b8a75423ac096874f52fdc52c3a6
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-13 14:31:30 +0200

    Add / Remove option to / from guess row

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { useAppState } from '../context/AppStateContext';
+import { SOCKET_NUMBER } from '../shared';
 
 interface RowProps {
-  id: number | string;
+  id: string;
 }
 
 export const Row = ({ id }: RowProps) => {
+  const {
+    state: { currentGuess, currentRowIndex },
+  } = useAppState();
+
   return (
-    <div className="row" id={id.toString()}>
+    <div className="row" id={id}>
       <span className="row-numeracy">{id}.</span>
       <div className="row-sockets">
-        <div className="row-socket"></div>
-        <div className="row-socket"></div>
-        <div className="row-socket"></div>
-        <div className="row-socket"></div>
+        {[...Array(SOCKET_NUMBER)].map((_, idx) => {
+          if (currentRowIndex === +id && currentGuess[idx]) {
+            return (
+              <div key={idx} className="row-socket row-socket-filled">
+                {currentGuess[idx]}
+              </div>
+            );
+          }
+          return <div key={idx} className="row-socket"></div>;
+        })}
       </div>
       <div className="row-hints">
         <div className="row-hint"></div>
--- src/components/UserControlPanel.tsx
+++ src/components/UserControlPanel.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import { OPTIONS } from '../shared';
+import { useAppState } from '../context/AppStateContext';
 
 export const UserControlPanel = () => {
+  const { state, dispatch } = useAppState();
+
+  const addToGuess = (id: string): void => {
+    if (state.currentGuess.length < 4) {
+      dispatch({ type: 'ADD_GUESS', payload: id });
+    }
+
+    return;
+  };
+
+  const undoOptionFromGuess = (): void => {
+    if (state.currentGuess.length > 0) {
+      dispatch({ type: 'UNDO_GUESS' });
+    }
+
+    return;
+  };
+
   return (
     <div className="user-control">
       <div className="options">
-        {[...Array(6)].map((_, idx) => (
-          <button key={5 - idx} className="option" id={(5 - idx).toString()}>
-            {idx}
+        {OPTIONS.map((item, idx) => (
+          <button key={idx} className="option" id={item} onClick={() => addToGuess(item)}>
+            {item}
           </button>
         ))}
       </div>
-      <button className="undo">
+      <button className="undo" onClick={() => undoOptionFromGuess()}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path d="M142.716 293.147l-94-107.602 94-107.602c7.596-8.705 6.71-21.924-1.995-29.527-8.705-7.596-21.917-6.703-29.527 1.995L5.169 171.782c-6.892 7.882-6.892 19.65 0 27.532l106.026 121.372a20.913 20.913 0 0015.771 7.157 20.84 20.84 0 0013.755-5.169c8.706-7.603 9.598-20.822 1.995-29.527z" />
           <path d="M359.93 164.619H20.926C9.368 164.619 0 173.986 0 185.545c0 11.558 9.368 20.926 20.926 20.926H359.93c60.776 0 110.218 49.441 110.218 110.211S420.706 426.893 359.93 426.893H48.828c-11.558 0-20.926 9.368-20.926 20.926 0 11.558 9.368 20.926 20.926 20.926H359.93c83.844 0 152.07-68.219 152.07-152.063s-68.219-152.063-152.07-152.063z" />
