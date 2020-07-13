1b0795291d1da31938a987ce9123d5b27b117375
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-13 14:30:38 +0200

    Temporarly log state data on screen

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { Row } from './Row';
 import { SecretCode } from './SecretCode';
 import { UserControlPanel } from './UserControlPanel';
-
-const ROW_NUMBER = 8;
+import { ROW_NUMBER } from '../shared';
+import { useAppState } from '../context/AppStateContext';
 
 export const Board = () => {
+  const { state } = useAppState();
+
   return (
     <>
+      <p>
+        SecretCode: {state.secretCode} | Guess: {state.currentGuess} | Row: {state.currentRowIndex}
+      </p>
       <div className="board">
         <SecretCode />
         <div className="board-rows">
           {[...Array(ROW_NUMBER)].map((_, idx) => (
-            <Row key={ROW_NUMBER - idx} id={ROW_NUMBER - idx} />
+            <Row key={ROW_NUMBER - idx} id={(ROW_NUMBER - idx).toString()} />
           ))}
         </div>
       </div>
