75086e1019a365c0832e50e5462cb80126933e17
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-15 17:22:48 +0200

    Render rows based on history

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -8,24 +8,44 @@ interface RowProps {
 
 export const Row = ({ id }: RowProps) => {
   const {
-    state: { currentGuess, currentRowIndex },
+    state: { currentGuess, currentRowIndex, history },
   } = useAppState();
+
+  const renderSockets = (rowId: string): JSX.Element[] => {
+    // convert rowId to number to use it as index on the array
+    // -1 because rows ids are counted from 1, not 0 as in arrays
+    const id = +rowId - 1;
+
+    // render guesses from state.history for previous rows
+    if (history[id]) {
+      const prevGuess = history[id].guess;
+      return prevGuess.map((_item, idx) => {
+        return (
+          <div key={idx} className="row-socket row-socket-filled">
+            {prevGuess[idx]}
+          </div>
+        );
+      });
+    } else {
+      //  render current guess sockets
+      return [...Array(SOCKET_NUMBER)].map((_item, idx) => {
+        if (currentRowIndex === id + 1 && typeof currentGuess[idx] !== 'undefined') {
+          return (
+            <div key={idx} className="row-socket row-socket-filled">
+              {currentGuess[idx]}
+            </div>
+          );
+        }
+        // default: return the rest of empty sockets
+        return <div key={idx} className="row-socket"></div>;
+      });
+    }
+  };
 
   return (
     <div className="row" id={id}>
       <span className="row-numeracy">{id}.</span>
-      <div className="row-sockets">
-        {[...Array(SOCKET_NUMBER)].map((_, idx) => {
-          if (currentRowIndex === +id && currentGuess[idx]) {
-            return (
-              <div key={idx} className="row-socket row-socket-filled">
-                {currentGuess[idx]}
-              </div>
-            );
-          }
-          return <div key={idx} className="row-socket"></div>;
-        })}
-      </div>
+      <div className="row-sockets">{renderSockets(id)}</div>
       <div className="row-hints">
         <div className="row-hint"></div>
         <div className="row-hint"></div>
