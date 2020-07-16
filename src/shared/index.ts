99a800565d46ec907ed094a53a06e8c1a03b2e44
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-16 23:33:14 +0200

    Switch from options array to single int

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,11 +1,8 @@
-// ROW_NUMBER === maximum guesses by user before game ends
-export const ROW_NUMBER = 8;
+export const ROWS_NUMBER = 8;
 
-// length of code (guess)
-export const SOCKET_NUMBER = 4;
+export const SOCKETS_NUMBER = 4;
 
-// Array of strings instead of numbers to avoid stringifying ids in the future
-export const OPTIONS = ['0', '1', '2', '3', '4', '5'];
+export const OPTIONS_NUMBER = 6;
 
 export interface Hints {
   black: number;
