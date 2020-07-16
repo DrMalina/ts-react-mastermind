9e8e63fb9382cda57c775620574272c7cd054073
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-16 23:34:47 +0200

    Temporarily log win state

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,22 +2,38 @@ import React from 'react';
 import { Row } from './Row';
 import { SecretCode } from './SecretCode';
 import { UserControlPanel } from './UserControlPanel';
-import { ROW_NUMBER } from '../shared';
+import { ROWS_NUMBER } from '../shared';
 import { useAppState } from '../context/AppStateContext';
 
 export const Board = () => {
   const { state } = useAppState();
 
+  const logWin = () => {
+    switch (state.isWin) {
+      case true: {
+        return 'true';
+      }
+      case false: {
+        return 'false';
+      }
+
+      default: {
+        return 'undefined';
+      }
+    }
+  };
+
   return (
     <>
       <p>
-        SecretCode: {state.secretCode} | Guess: {state.currentGuess} | Row: {state.currentRowIndex}
+        SecretCode: {state.secretCode} | Guess: {state.currentGuess} | Row: {state.currentRowIndex}{' '}
+        | isWin: {logWin()}
       </p>
       <div className="board">
         <SecretCode />
         <div className="board-rows">
-          {[...Array(ROW_NUMBER)].map((_, idx) => (
-            <Row key={ROW_NUMBER - idx} id={(ROW_NUMBER - idx).toString()} />
+          {[...Array(ROWS_NUMBER)].map((_, idx) => (
+            <Row key={ROWS_NUMBER - idx} id={(ROWS_NUMBER - idx).toString()} />
           ))}
         </div>
       </div>
