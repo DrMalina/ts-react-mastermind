0fa18f4220e602c9c4ee05a83224014c0f91d21d
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-17 18:54:20 +0200

    Reveal code sockets after game ends

--- src/components/SecretCode.tsx
+++ src/components/SecretCode.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import { SOCKETS_NUMBER } from '../shared';
+import { useAppState } from '../context/AppStateContext';
 
 export const SecretCode = () => {
+  const {
+    state: { isWin, secretCode },
+  } = useAppState();
+
+  const renderSockets = () => {
+    if (typeof isWin !== 'undefined') {
+      return secretCode.map((item, idx) => {
+        return (
+          <div key={idx} className="secret-code__socket row-socket__filled">
+            {item}
+          </div>
+        );
+      });
+    } else {
+      return [...Array(SOCKETS_NUMBER)].map((_item, idx) => {
+        return (
+          <div key={idx} className="secret-code__socket">
+            ?
+          </div>
+        );
+      });
+    }
+  };
+
   return (
     <div className="secret-code">
       <span>Secret code</span>
-      <div className="secret-code__sockets">
-        <div className="secret-code__socket">X</div>
-        <div className="secret-code__socket">X</div>
-        <div className="secret-code__socket">X</div>
-        <div className="secret-code__socket">X</div>
-      </div>
+      <div className="secret-code__sockets bold">{renderSockets()}</div>
     </div>
   );
 };
