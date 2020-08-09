1876200cf83783ec8a0c5dea6291c8340bfec6e6
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-09 18:36:59 +0200

    Update compareCode fn to work for edge cases when secret code ends with same digits

--- src/utils/compareCode.spec.ts
+++ src/utils/compareCode.spec.ts
@@ -9,23 +9,42 @@ describe('compare guess to a secret code', () => {
 
   test('should return 0 black, 4 white', () => {
     expect(compareCode(secretCode, [4, 2, 3, 1])).toStrictEqual({ black: 0, white: 4 });
+    expect(compareCode([1, 2, 3, 4], [2, 1, 4, 3])).toStrictEqual({ black: 0, white: 4 });
+    expect(compareCode([1, 1, 3, 4], [3, 4, 1, 1])).toStrictEqual({ black: 0, white: 4 });
   });
 
   test('should return 0 black, 0 white', () => {
     expect(compareCode(secretCode, [6, 6, 6, 6])).toStrictEqual({ black: 0, white: 0 });
   });
 
-  test('should return 2 black, 2 white', () => {
-    expect(compareCode(secretCode, [4, 3, 2, 1])).toStrictEqual({ black: 2, white: 2 });
+  test('should return 0 black, 1 white', () => {
+    expect(compareCode(secretCode, [0, 1, 1, 1])).toStrictEqual({ black: 0, white: 1 });
+    expect(compareCode([1, 1, 2, 4], [0, 0, 0, 1])).toStrictEqual({ black: 0, white: 1 });
+    expect(compareCode([1, 2, 3, 4], [3, 3, 0, 3])).toStrictEqual({ black: 0, white: 1 });
   });
 
   test('should return 1 black, 0 white', () => {
     expect(compareCode(secretCode, [1, 1, 1, 1])).toStrictEqual({ black: 1, white: 0 });
-    expect(compareCode([1, 1, 2, 4], [1, 0, 0, 0])).toStrictEqual({ black: 1, white: 0 });
+    expect(compareCode([0, 0, 0, 1], [1, 1, 1, 1])).toStrictEqual({ black: 1, white: 0 });
+    expect(compareCode([1, 1, 2, 4], [0, 1, 0, 0])).toStrictEqual({ black: 1, white: 0 });
+    expect(compareCode([1, 1, 2, 4], [2, 0, 2, 0])).toStrictEqual({ black: 1, white: 0 });
   });
 
-  test('should return 0 black, 1 white', () => {
-    expect(compareCode(secretCode, [0, 1, 1, 1])).toStrictEqual({ black: 0, white: 1 });
-    expect(compareCode([1, 1, 2, 4], [0, 0, 0, 1])).toStrictEqual({ black: 0, white: 1 });
+  test('should return 1 black, 1 white', () => {
+    expect(compareCode([1, 2, 2, 3], [3, 2, 4, 5])).toStrictEqual({ black: 1, white: 1 });
+    expect(compareCode([4, 1, 2, 2], [0, 3, 4, 2])).toStrictEqual({ black: 1, white: 1 });
+  });
+
+  test('should return 1 black, 2 white', () => {
+    expect(compareCode([4, 1, 2, 2], [1, 4, 5, 2])).toStrictEqual({ black: 1, white: 2 });
+    expect(compareCode([2, 3, 1, 4], [3, 4, 1, 0])).toStrictEqual({ black: 1, white: 2 });
+  });
+
+  test('should return 2 black, 2 white', () => {
+    expect(compareCode(secretCode, [4, 3, 2, 1])).toStrictEqual({ black: 2, white: 2 });
+  });
+
+  test('should return 1 black, 3 white', () => {
+    expect(compareCode([1, 3, 4, 5], [4, 3, 5, 1])).toStrictEqual({ black: 1, white: 3 });
   });
 });
--- src/utils/compareCode.ts
+++ src/utils/compareCode.ts
@@ -2,20 +2,25 @@ import { Hints } from '../shared/';
 
 export const compareCode = (code: number[], guess: number[]): Hints => {
   const guessCopy = [...guess];
+  const codeCopy = [...code];
+  const results = { black: 0, white: 0 };
 
-  return code.reduce(
-    (acc, item, idx) => {
-      if (guessCopy.includes(item)) {
-        if (code[idx] === guessCopy[idx]) {
-          acc['black']++;
-          guessCopy[idx] = -1; // to not count that same element again in next iteration
-        } else {
-          acc['white']++;
-          guessCopy[guessCopy.indexOf(item)] = -1;
-        }
-      }
-      return acc;
-    },
-    { black: 0, white: 0 },
-  );
+  // First check all RIGHT PEGS that are in RIGHT SPOT
+  for (let i = 0; i < code.length; i++) {
+    if (codeCopy[i] === guessCopy[i]) {
+      results.black++;
+      // set negative values to not count those spots in other iterations
+      codeCopy[i] = -1;
+      guessCopy[i] = -2;
+    }
+  }
+
+  // Then check if there are any right pegs left (digits) but NOT in right spots
+  for (let j = 0; j < code.length; j++) {
+    if (guessCopy.includes(codeCopy[j])) {
+      results.white++;
+      guessCopy[guessCopy.indexOf(codeCopy[j])] = -2;
+    }
+  }
+  return results;
 };
